8540cbfcff680461a17a2bcf0868bf57dc671b84
FauzanHasibuan78 <fauzanhasibuan78@gmail.com>
2023-10-10 10:37:45 +0700

    BE terbaru1

--- routes/tarifRoutes.js
+++ routes/tarifRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const  tarifControllers  = require("../controllers/tarifController");
 const tarifRoutes = express.Router();
 
-tarifRoutes.get("/tarif", tarifControllers.getTarif);
+tarifRoutes.get("/", tarifControllers.getTarif);
 
 
 module.exports = {tarifRoutes};
